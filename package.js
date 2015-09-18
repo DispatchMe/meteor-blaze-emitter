9aa3c66e7d73c774e40adcdf011e650e61d23dc1
Derek Caneja <derek@dispatch.me>
2015-09-18 18:12:29 -0400

    Bump and release to atmosphere

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'dispatch:blaze-emitter',
   summary: 'Emit template events',
-  version: '0.0.7',
+  version: '0.0.8',
   git: 'https://github.com/DispatchMe/meteor-blaze-emitter'
 });
 
